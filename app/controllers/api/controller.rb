b60961ac7958ebf793523bb71dd22f3b0a1f6186
Ibrahim Shendy <4webcoding@gmail.com>
2025-07-21 15:39:33 +0300

    Implement Idempotency layer for POST endpoints

--- app/controllers/api/controller.rb
+++ app/controllers/api/controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Api::Controller < ActionController::API
+  before_action :path_request_info
+  before_action :validate_idempotency_key, only: :create
+
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
 
   private
@@ -11,6 +14,28 @@ class Api::Controller < ActionController::API
     render json: {
       errors: record.errors.to_hash
     }, status: :unprocessable_entity
+  end
+
+  def path_request_info
+    BaseService.setup(
+      **{
+        path: request.path,
+        request_method: request.method,
+        ip_address: request.remote_ip,
+        idempotency_key: request.headers["Idempotency-Key"]
+      }
+    )
+  end
+
+  def validate_idempotency_key
+    key = request.headers["Idempotency-Key"]
+
+    # TODO : split into a single custom error class.
+    return render json: { error: "Missing idempotency key" }, status: :not_found if key.blank?
+
+    idempotency = IdempotencyService.find_by(key: key)
+
+    render json: idempotency.resource, status: idempotency.status_code if idempotency.present?
   end
 
 end
--- app/services/post_service.rb
+++ app/services/post_service.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
-class PostService
+class PostService < BaseService
+
   def self.create(attributes:)
-    post = Post.create! attributes
-    post
+    ActiveRecord::Base.transaction do
+      post = Post.create! attributes
+
+      # storing idempotency key
+      IdempotencyService.create attributes: {
+        key: self.idempotency_key, resource_type: Post.name, resource_id: post.id, status_code: 201
+      }
+
+      post
+    end
   end
 
   def self.update(post:, attributes:)
